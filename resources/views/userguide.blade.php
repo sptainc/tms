c251db2afce766d90fe7d7a46bcf6c2ff021fc0c
Tai Nguyen <tainc.ftcjsc@gmail.com>
2022-03-10 14:02:02 +0700

    show markdown

--- resources/views/userguide.blade.php
+++ resources/views/userguide.blade.php
@@ -32,7 +32,9 @@
     <div class="container-fluid">
         <main class="main section-color-primary">
             <div class="container">
+                <x-markdown>
                 {!! isset($content) && $content != null ? $content->content : "Không có dữ liệu" !!}
+                </x-markdown>
             </div>
         </main>
     </div>
