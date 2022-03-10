ec07fba677d33610970db1b3f35fd6da477f9c9c
Tai Nguyen <tainc.ftcjsc@gmail.com>
2022-03-10 15:30:41 +0700

    update

--- app/Admin/Apis/UserGuideApi.php
+++ app/Admin/Apis/UserGuideApi.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Admin\Apis;
 
-use App\Question;
+use App\UserGuide;
 use Illuminate\Http\Request;
 
 class UserGuideApi
@@ -9,19 +9,19 @@ class UserGuideApi
 	public function getAll(Request $req) 
 	{
 		$q = $req->get('q');
-		return Question::where('title', 'like', "%$q%")->paginate(null, ['id', 'title as text']);
+		return UserGuide::where('title', 'like', "%$q%")->paginate(null, ['id', 'title as text']);
 	}
 
 	public function getByUri ( Request $req )
 	{
-		$parent = Question::select('id')->where([
+		$parent = UserGuide::select('id')->where([
 			['uri', 'like', '%' . $req->get('q') . '%'],
 			['parent_id', null]
 		])->first();
 		if ( $parent )
-			$questions = Question::where('parent_id', $parent->id)->orderBy('updated_at', 'desc')->get();
+			$questions = UserGuide::where('parent_id', $parent->id)->orderBy('updated_at', 'desc')->get();
 		else 
-			$questions = Question::where('uri', 'like', '%' . $req->get('q') . '%')->orderBy('updated_at', 'desc')->get();
+			$questions = UserGuide::where('uri', 'like', '%' . $req->get('q') . '%')->orderBy('updated_at', 'desc')->get();
 
 		return response()->json([
                 'success' => true,
