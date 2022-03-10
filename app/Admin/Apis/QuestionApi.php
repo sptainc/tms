9045c98a900310455887945dafcb4530ff8e36c5
Tai Nguyen <tainc.ftcjsc@gmail.com>
2022-03-10 15:11:03 +0700

    update

--- app/Admin/Apis/QuestionApi.php
+++ app/Admin/Apis/QuestionApi.php
@@ -2,6 +2,7 @@
 namespace App\Admin\Apis;
 
 use App\Question;
+use App\UserGuide;
 use Illuminate\Http\Request;
 
 class QuestionApi
@@ -18,14 +19,19 @@ class QuestionApi
 			['uri', 'like', '%' . $req->get('q') . '%'],
 			['parent_id', null]
 		])->first();
-		if ( $parent )
+		if ( $parent ) {
 			$questions = Question::where('parent_id', $parent->id)->orderBy('updated_at', 'desc')->get();
-		else 
+			$userGuide = UserGuide::where('parent_id', $parent->id)->orderBy('updated_at', 'desc')->get();
+		}
+		else {
 			$questions = Question::where('uri', 'like', '%' . $req->get('q') . '%')->orderBy('updated_at', 'desc')->get();
+			$userGuide = UserGuide::where('uri', 'like', '%' . $req->get('q') . '%')->orderBy('updated_at', 'desc')->get();
+		}
 
 		return response()->json([
                 'success' => true,
                 'questions' => $questions
+                'user-guide' => $userGuide
             ], 200 );
 	}
 }
