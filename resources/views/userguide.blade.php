e27dea5ad4052dde6a4deb6ed447cba98929a114
Tai Nguyen <tainc.ftcjsc@gmail.com>
2022-03-10 14:32:29 +0700

    set witdh image

--- resources/views/userguide.blade.php
+++ resources/views/userguide.blade.php
@@ -41,6 +41,13 @@
             text-align: center;
         }
 
+        blockquote > p{
+            border-style: solid;
+            border-width: 2px 0 2px 4px;
+            border-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAFCAYAAABirU3bAAAAOklEQVQI103IQQ2AMBjF4G+/DPCEjiXYwdnTMEAGB3ZYkx7aluTA5edsSQa2OUYtAXvhWcZd6Hin/QOIPwzOiksIkAAAAABJRU5ErkJggg==) 2 0 2 4;
+            padding-left: 0.5em;
+        }
+
     </style>
 </head>
 
